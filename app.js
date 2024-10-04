4486e2cd0e02f82c5872abab9761a84162a747dd
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-10-04 21:03:04 +0530

    for loop tested

---
+++ app.js
@@ -0,0 +1,7 @@
+// //__________Loops_____________//
+// for (let i = 0; i <= 10; i++) {
+//   if (i % 2 === 0) console.log("even number:", i);
+//   else {
+//     console.log("odd number", i);
+//   }
+// }
