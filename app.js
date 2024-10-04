eb5eafb7fd9bc40bef99391241afa7347c1374c7
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-10-04 23:28:55 +0530

    simple function examples

--- app.js
+++ app.js
@@ -5,3 +5,12 @@
 //     console.log("odd number", i);
 //   }
 // }
+//________normal function____________//
+function hello() {
+  console.log("hello everyone");
+}
+hello(); //________function call______//
+
+//_______________Arrorw function_________________//
+let sum = (a, b) => a + b;
+console.log(sum(5, 7));
