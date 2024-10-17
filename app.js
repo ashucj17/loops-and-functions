a2a5b9db51b7767c01232ad8ab6260039b91fc80
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-10-17 23:11:36 +0530

    logical operatiors

--- app.js
+++ app.js
@@ -5,23 +5,62 @@
 //     console.log("odd number", i);
 //   }
 // }
-//________normal function____________//
-function hello() {
-  console.log("hello everyone");
-}
-hello(); //________function call______//
+// //________normal function____________//
+// function hello() {
+//   console.log("hello everyone");
+// }
+// hello(); //________function call______//
 
-//_______________Arrorw function_________________//
-let sum = (a, b) => a + b;
-console.log(sum(5, 7));
+// //_______________Arrorw function_________________//
+// let sum = (a, b) => a + b;
+// console.log(sum(5, 7));
 
-let num = (a) => a * a;
-{
-  console.log(num(21));
-}
+// let num = (a) => a * a;
+// {
+//   console.log(num(21));
+// }
 
-let mod = (a, b) => a / b;
-console.log(mod(3, 4));
+// let mod = (a, b) => a / b;
+// console.log(mod(3, 4));
 
-let sub = (a, b) => a - b;
-console.log(sub(21, 13));
+// let sub = (a, b) => a - b;
+// console.log(sub(21, 13));
+
+// for (let i = 0; i <= 1000; i++) {
+//   console.log("Ashish", i);
+// }
+
+// whioe loop
+// let i = 10;
+// while (i <= 100) {
+//   console.log("ashish", i);
+//   i++;
+// }
+
+// do while
+
+// let i = 20;
+// do {
+//   console.log("ashish", i);
+//   i++;
+// } while (i <= 100);
+
+// logical operators
+// && and operator
+// let age = 18;
+// if (age >= 18 && age <= 60) {
+//   console.log("Person can drive", age);
+// } else {
+//   console.log("person cant drive");
+// }
+// || or operator
+// if (age < 18 || age === 17) {
+//   console.log("person cannot vote");
+// } else {
+//   console.log("person can vote");
+// }
+
+// ! not operator
+// let a = true;
+// let b = false;
+// console.log(!a);
