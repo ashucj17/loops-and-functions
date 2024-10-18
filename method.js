dc374a24ea21110c12b5f75eeadae5ade42ace96
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-10-18 21:27:23 +0530

    Created basic javascript methods

---
+++ method.js
@@ -0,0 +1,20 @@
+// function greating() {
+//   return `hello my name is ${person.pName} i am ${person.age} years old`;
+// }
+// let person = {
+//   pName: "ashu",
+//   age: 29,
+//   greating,
+// };
+// console.log(person.greating());
+
+// let person = {
+//   pName: "Rakesh",
+//   age: 27,
+//   location: "Satna",
+//   great,
+// };
+// function great() {
+//   return `Hello my name is ${person.pName} i am ${person.age} years old. I from ${person.location}.`;
+// }
+// console.log(person.great());
