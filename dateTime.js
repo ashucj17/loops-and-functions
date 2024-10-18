c44569a009f2e3f87ef8f36f059888a2101d4b38
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2024-10-18 23:04:21 +0530

    date and time methods

---
+++ dateTime.js
@@ -0,0 +1,22 @@
+//  Date and time methods
+
+const date = new Date();
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleString());
+
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getMonth();
+const hour = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const milliSeconds = date.getMilliseconds();
+
+console.log(`Year: ${year}`);
+console.log(`Month: ${month}`);
+console.log(`Day: ${day}`);
+console.log(`Hour: ${hour}`);
+console.log(`Minutes: ${minutes}`);
+console.log(`Seconds: ${seconds}`);
+console.log(`MiliSeconds: ${milliSeconds}`);
